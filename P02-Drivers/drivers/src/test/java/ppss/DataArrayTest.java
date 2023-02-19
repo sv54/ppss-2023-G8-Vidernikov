0dc7c45f427fed85734033d05ae579650c2d3cdc
Serhii Vidernikov <sv54@alu.ua.es>
2023-02-19 13:35:26 +0100

    Ejercicio 4 C) hecho

--- P02-Drivers/drivers/src/test/java/ppss/DataArrayTest.java
+++ P02-Drivers/drivers/src/test/java/ppss/DataArrayTest.java
@@ -1,7 +1,14 @@
 package ppss;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DataArrayTest {
@@ -70,4 +77,24 @@ public class DataArrayTest {
         DataException dataException = assertThrows(DataException.class, () -> dataArray.delete(borrar));
         assertEquals("Elemento no encontrado", dataException.getMessage());
     }
+
+    @Tag("parametrizado")
+    @Tag("conExcepciones")
+    @ParameterizedTest
+    @MethodSource("casosExcepciones")
+    public void dataArrayTestC8(String mensajeEsperado, int borrar,int[] coleccion ){
+        dataArray = new DataArray(coleccion);
+        DataException dataException = assertThrows(DataException.class, () -> dataArray.delete(borrar));
+        assertEquals(mensajeEsperado, dataException.getMessage());
+    }
+
+    public static Stream<Arguments> casosExcepciones(){
+        return Stream.of(
+                Arguments.of("No hay elementos en la colección",8,new int[]{}),
+                Arguments.of("El valor a borrar debe ser > 0",-5, new int[]{1,3,5,7}),
+                Arguments.of("Colección vacía. Y el valor a borrar debe ser > 0",0, new int[]{}),
+                Arguments.of("Elemento no encontrado",8, new int[]{1,3,5,7})
+        );
+    }
+
 }
